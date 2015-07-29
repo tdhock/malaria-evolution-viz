79d3fd6d75784fb5ac7a21e28e8b20ad39260344
Toby Dylan Hocking <tdhock5@gmail.com>
2015-07-29 17:03:31 -0400

    column descriptions

--- variants.R
+++ variants.R
@@ -7,3 +7,35 @@ unfiltered <- fread("N67toYMaug_22Feb13_reflist_2June14_eBAQ_MASTER_NOChr2_fullh
 ## not detected at all.
 
 
+## "#CHROM - Chromosome name from the reference genome, in the format
+## of PyYM_##_v1.  The naming convention is organism (genus + species,
+## Plasmodium yoelii in this case, Py) then isolate (YM) and the
+## version (1).  The hash is just there to easily grep out the header,
+## etc.
+
+## POS - Position of the variant in the reference, in the case of
+## indels this is the start position.
+
+## LOCUS_ID - a unique identifier for each amplicon (locus), named for
+## the chromosome and then sequentially.
+
+## Variant_type --binary variable, SNP or INDEL
+
+## DP - Coverage depth
+
+## QUAL - Phred-scaled quality score
+
+## MQ - Mapping Quality
+
+## FQ - Consensus Quality
+
+## HDR/LCR - Whether the variant position is with in an HDR (highly
+## divergent regions) or LCR (low-complexity region), otherwise (.)
+
+## Validation - TP (true positive), FP (false positive), or FN (False
+## Negative)
+
+##  Error - Boolean (True for FN and FP, False for TP)
+
+## Coding - Whether the variant is coding, intergenic, or in an
+## intron.
